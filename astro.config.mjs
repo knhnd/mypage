9465a7eecc86940d193ace872db8bc79fc421b11
Ken Honda <ken.h.0802@gmail.com>
2024-06-28 13:19:44 +0900

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://knhnd.github.io',
+  base: '/mypage/',
   integrations: [mdx(), sitemap(), tailwind()],
 });
